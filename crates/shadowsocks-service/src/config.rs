feb5df8648d495a1aa7e20e88b5cc27fab9fe140
samanajd2 <59361452+samanajd2@users.noreply.github.com>
2024-11-17 03:10:47 +0400

    Update config.rs

--- crates/shadowsocks-service/src/config.rs
+++ crates/shadowsocks-service/src/config.rs
@@ -2065,17 +2065,7 @@ impl Config {
                     }
                 };
 
-                let mut nsvr = match ServerConfig::new(addr, password, method) {
-                    Ok(svr) => svr,
-                    Err(serr) => {
-                        let err = Error::new(
-                            ErrorKind::Malformed,
-                            "server config create failed",
-                            Some(format!("{}", serr)),
-                        );
-                        return Err(err);
-                    }
-                };
+                let mut nsvr = ServerConfig::new(addr, password, method);
                 nsvr.set_source(server_source);
 
                 // Extensible Identity Header, Users
